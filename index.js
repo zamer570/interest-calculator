7c921127cd3c3734d1cb663877e7b66f771d3269
apple <zamersbouy@gmail.com>
2023-12-29 17:21:50 +0530

    changes

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.get("/interest",(req,res)=>{
     const {principal,rate,time} = req.query;
     let si = ((parseInt(principal) * parseInt(rate) * parseInt(time))/100);
     res.json(si);
-})
+});
 
-app.listen(3000,()=>{
-    console.log("app is listening on port 3000");
-})
+
+app.listen(3000);
